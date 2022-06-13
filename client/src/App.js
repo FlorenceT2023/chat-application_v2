a834cd1dca9dbb8dcbd1a1bbd068db2e708a0bf2
Florence Tat <florence.tat28@ed.cna.nl.ca>
2022-06-13 16:18:42 -0230

    installed packages and basic functionality

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import io from 'socket.io-client';
+import { useState } from 'react';
+
+const socket = io.connect("http://localhost:3001"); // connects to server/back-end components
+
+function App() {
+  // sets state for username and chatroom
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room)
+    }
+  };
+
+  return (
+    <div className="App">
+      <h3>Join a Chat</h3>
+      <input 
+        type="text" 
+        placeholder="Flo..." 
+        onChange = {(event) => { 
+          setUsername(event.target.value);
+        }} 
+      />
+      <input 
+        type="text" 
+        placeholder="Room ID..." 
+        onChange = {(event) => { 
+          setRoom(event.target.value);
+        }} />
+      <button onClick={joinRoom}>Join a Room</button>
+    </div>
+  );
+}
+
+export default App;
